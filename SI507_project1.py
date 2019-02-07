f0a2a01182488d4763ce1469f96659228a042e63
zoey <zuyicai@umich.edu>
2019-02-06 20:23:49 -0500

    the final version- modify the instance of each route

--- SI507_project1.py
+++ SI507_project1.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from lab3_code import *
+# import statements
 
 # Set up application
 app = Flask(__name__)
@@ -7,57 +8,54 @@ app = Flask(__name__)
 
 # Routes
 @app.route('/')#http://127.0.0.1:5000/
-def home_page(): # Route function names can be anything unique
-    return '<h1>Welcome to the banking application!</h1>' # Route functions can return plain old text, or text in correctly formatted HTML (or the return values of many special functions we haven't looked at yet)
-
-# Something else to note is that the functions don't get INVOKED the way you may be used to. Running the application and *going to a URL that matches the path in the @app.route() business IS what runs that function!
+def home_page():
+    return '<h1>Hi! Welcome to Zuyi Cai\'s banking application!</h1>'
 
 
 #amazing = Bank("Amazing New Bank", Dollar, 1)
 @app.route('/bank/<name>')#http://127.0.0.1:5000/bank/Amazing%20New%20Bank
 def welcomebank(name):
-    return '<h1>Welcome to {}!</h1>'.format(name)
+    bank = Bank(name, Dollar, 0)
+    return '<h1>Welcome to {}!</h1>'.format(bank.name)
 
 
 #twentyfive_dollars = Dollar(25)
 @app.route('/dollar/<amt>')#http://127.0.0.1:5000/dollar/25
 def dollaramt(amt):
-    if int(amt) <=1:
-        return '<h1>{} Dollar</h1>'.format(amt)
-    else:
-        return '<h1>{} Dollars</h1>'.format(amt)
+    dollar = Dollar(int(amt))
+    return '<h1>NUMBER Dollars /{} {}</h1>'.format(dollar.value,dollar.unit_name)
     #number dollars
 
 
 
 @app.route('/yuan/<amt>')#http://127.0.0.1:5000/yuan/1
 def yuanamt(amt):
-    return '<h1>{} Yuan</h1>'.format(int(amt))
+    yuan = Yuan(int(amt))
+    return '<h1>NUMBER Yuan /{} {}</h1>'.format(yuan.value,yuan.unit_name)
 
 
 
-@app.route('/pound/<amt>')
+@app.route('/pound/<amt>')#http://127.0.0.1:5000/pound/100
 def poundamt(amt):
-    if int(amt) <=1:
-        return '<h1>{} Pound</h1>'.format(amt)
-    else:
-        return '<h1>{} Pounds</h1>'.format(amt)
+    pound = Pound(int(amt))
+    return '<h1>NUMBER Pounds /{} {}</h1>'.format(pound.value,pound.unit_name)
 
 
 #the last part
 #FifthThird = Bank("FifthThird", Dollar, 1000)
 #Amazing = Bank("A%20m%20a%20z%20i%20n%20g", Yuan, 17600)
-@app.route('/bank/<name>/<currency>/<value>')
-def ability(name, currency, value):
-    if currency == 'Dollar':
-        currency = 'Dollar'
-    elif currency == 'Yuan':
-        currency = 'Yuan'
-    elif currency == 'Pound':
-        currency = 'Pound'
+@app.route('/bank/<name>/<currency>/<value>')#http://127.0.0.1:5000/bank/A%20m%20a%20z%20i%20n%20g/Yuan/17600
+def bank_ability(name, currency, value):
+    validcurrency = ['Dollar', 'Yuan', 'Pound']
+    if currency == valid_urrency[0]:
+        bank = Bank(name, Dollar, int(value))
+    elif currency == validcurrency[1]:
+        bank = Bank(name, Yuan, int(value))
+    elif currency == validcurrency[2]:
+        bank = Bank(name, Pound, int(value))
     else:
         return '<h1>Invalid URL inputs for bank.</h1>'
-    return '<h1>Welcome to the {} bank! {} Bank holds the {} currency and currently holds {} of {}.</h1>'.format(name, name, currency, value, currency)
+    return '<h1>Welcome to the {} bank! {} Bank holds the {} currency and currently holds {} of {}.</h1>'.format(bank.name, bank.name, bank.current_account.unit_name, bank.current_account.value, bank.current_account.unit_name)
         #Welcome to the NAME bank! NAME Bank holds the CURRENCY currency and currently holds VALUE of CURRENCY.
 
 
